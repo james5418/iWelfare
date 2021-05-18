539d1d9e3f89656dd8bbef98c1d90e0d81568203
aurora <cscs223.cs08@nctu.edu.tw>
2021-05-19 02:19:26 +0800

    disability table added

---
+++ table/disability_create.sql
@@ -0,0 +1,5 @@
+CREATE TABLE disability(
+	id int NOT NULL,
+	disability varchar(5),
+	PRIMARY KEY(id)
+);
--- table/income_create.sql
+++ table/income_create.sql
@@ -1,6 +1,6 @@
 CREATE TABLE income(
 	id int NOT NULL,
-	low-income int,
-	low_and_middle-income int,
+	low_income int,
+	low_and_middle_income int,
 	PRIMARY KEY(id)
 );

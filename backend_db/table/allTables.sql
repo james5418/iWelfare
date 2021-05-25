05b7beddc0573bb26843d00370e1d2e29e51ca86
cindy203cc <cindy203cc@gmail.com>
2021-05-25 16:42:11 +0800

    add_corresponding

--- backend_db/table/allTables.sql
+++ backend_db/table/allTables.sql
@@ -107,3 +107,15 @@ fields terminated by ','
 enclosed by '"'
 lines terminated by '\n'
 ignore 1 lines;
+
+create table corresponding(
+	welfare_id int NOT NULL,
+	tag_id int NOT NULL
+);
+
+load data local infile './corresponding.csv'
+into table corresponding
+fields terminated by ','
+enclosed by '"'
+lines terminated by '\n'
+ignore 1 lines;

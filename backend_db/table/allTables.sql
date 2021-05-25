6991abd893854c740e1467f084c5f7647be50071
aurora <cscs223.cs08@nctu.edu.tw>
2021-05-25 17:41:52 +0800

    add overall table and allTable.sql

--- backend_db/table/allTables.sql
+++ backend_db/table/allTables.sql
@@ -1,3 +1,9 @@
+DROP TABLE age;
+DROP TABLE corresponding;
+DROP TABLE tags;
+DROP TABLE overall;
+
+
 create table age(
 	welfare_id int NOT NULL,
 	age_lower int,
@@ -13,86 +19,18 @@ lines terminated by '\n'
 ignore 1 lines;
 
 
-create table other(
+create table corresponding(
 	welfare_id int NOT NULL,
-	tag1 varchar(5),
-	tag2 varchar(5),
-	tag3 varchar(5),
-	tag4 varchar(5),
-	tag5 varchar(5),
-	tag6 varchar(5),
-	tag7 varchar(5),
-	tag8 varchar(5),
-	tag9 varchar(5),
-	tag10 varchar(5),
-	tag11 varchar(5),
-	tag12 varchar(5),
-	tag13 varchar(5),
-	tag14 varchar(5),
-	tag15 varchar(5),
-	tag16 varchar(5),
-	tag17 varchar(5),
-	tag18 varchar(5),
-	tag19 varchar(5),
-	tag20 varchar(5),
-	tag21 varchar(5),
-	tag22 varchar(5),
-	tag23 varchar(5),
-	tag24 varchar(5),
-	tag25 varchar(5),
-	tag26 varchar(5),
-	tag27 varchar(5),
-	tag28 varchar(5),
-	tag29 varchar(5),
-	tag30 varchar(5),
-	tag31 varchar(5),
-	tag32 varchar(5),
-	tag33 varchar(5),
-	tag34 varchar(5),
-	tag35 varchar(5),
-	tag36 varchar(5),
-	tag37 varchar(5),
-	tag38 varchar(5),
-	tag39 varchar(5),
-	tag40 varchar(5),
-	tag41 varchar(5),
-	tag42 varchar(5),
-	tag43 varchar(5),
-	tag44 varchar(5),
-	tag45 varchar(5),
-	tag46 varchar(5),
-	tag47 varchar(5),
-	tag48 varchar(5),
-	tag49 varchar(5),
-	tag50 varchar(5),
-	tag51 varchar(5),
-	tag52 varchar(5),
-	tag53 varchar(5),
-	tag54 varchar(5),
-	tag55 varchar(5),
-	PRIMARY KEY(welfare_id)
+	tag_id int NOT NULL
 );
 
-load data local infile './other.csv'
-into table other
+load data local infile './corresponding.csv'
+into table corresponding
 fields terminated by ','
 enclosed by '"'
 lines terminated by '\n'
 ignore 1 lines;
 
-
-create table region(
-	welfare_id int NOT NULL,
-	city text,
-	PRIMARY KEY(welfare_id)
-);
-
-load data local infile './region.csv'
-into table region
-fields terminated by ','
-enclosed by '"'
-lines terminated by '\n'
-ignore 1 lines;
 
 
 create table tags(
@@ -108,13 +46,22 @@ enclosed by '"'
 lines terminated by '\n'
 ignore 1 lines;
 
-create table corresponding(
+
+
+create table overall(
 	welfare_id int NOT NULL,
-	tag_id int NOT NULL
+	name text,
+	welfare text,
+	application_agency text,
+	contact_info text,
+	criteria text,
+	document_needed text,
+	notice text,
+	PRIMARY KEY(welfare_id)
 );
 
-load data local infile './corresponding.csv'
-into table corresponding
+load data local infile './overall.csv'
+into table overall
 fields terminated by ','
 enclosed by '"'
 lines terminated by '\n'
---
+++ backend_db/table/overall_table.sql
@@ -0,0 +1,19 @@
+create table overall(
+	welfare_id int NOT NULL,
+	name text,
+	welfare text,
+	application_agency text,
+	contact_info text,
+	criteria text,
+	document_needed text,
+	notice text,
+	PRIMARY KEY(welfare_id)
+);
+
+load data local infile './overall.csv'
+into table overall
+fields terminated by ','
+enclosed by '"'
+lines terminated by '\n'
+ignore 1 lines;
+

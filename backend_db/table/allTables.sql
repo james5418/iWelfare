934bbd9182b742fe557b3557e9fc1e4dc4ce2ff5
aurora <cscs223.cs08@nctu.edu.tw>
2021-05-27 21:43:21 +0800

    add foreign key

--- backend_db/table/allTables.sql
+++ backend_db/table/allTables.sql
@@ -3,6 +3,24 @@ DROP TABLE corresponding;
 DROP TABLE tags;
 DROP TABLE overall;
 
+create table overall(
+	welfare_id int NOT NULL,
+	name text,
+	welfare text,
+	application_agency text,
+	contact_info text,
+	criteria text,
+	document_needed text,
+	notice text,
+	PRIMARY KEY(welfare_id)
+);
+
+load data local infile './overall.csv'
+into table overall
+fields terminated by ','
+enclosed by '"'
+lines terminated by '\n'
+ignore 1 lines;
 
 create table age(
 	welfare_id int NOT NULL,
@@ -13,19 +31,6 @@ create table age(
 
 load data local infile './age.csv'
 into table age
-fields terminated by ','
-enclosed by '"'
-lines terminated by '\n'
-ignore 1 lines;
-
-
-create table corresponding(
-	welfare_id int NOT NULL,
-	tag_id int NOT NULL
-);
-
-load data local infile './corresponding.csv'
-into table corresponding
 fields terminated by ','
 enclosed by '"'
 lines terminated by '\n'
@@ -47,22 +52,17 @@ lines terminated by '\n'
 ignore 1 lines;
 
 
-
-create table overall(
+create table corresponding(
 	welfare_id int NOT NULL,
-	name text,
-	welfare text,
-	application_agency text,
-	contact_info text,
-	criteria text,
-	document_needed text,
-	notice text,
-	PRIMARY KEY(welfare_id)
+	tag_id int NOT NULL,
+	FOREIGN KEY(welfare_id) REFERENCES overall(welfare_id),
+	FOREIGN KEY(tag_id) REFERENCES tags(tag_id)
 );
 
-load data local infile './overall.csv'
-into table overall
+load data local infile './corresponding.csv'
+into table corresponding
 fields terminated by ','
 enclosed by '"'
 lines terminated by '\n'
 ignore 1 lines;
+
